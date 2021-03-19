672164492d0e203c6d882ad347292fef503d1a18
eddiebarry <edgarmonis@gmail.com>
2021-03-19 11:34:13 +0530

    adding new data

--- app_test.py
+++ app_test.py
@@ -6,7 +6,7 @@ base_url="http://0.0.0.0:5000/get-chitchat"
 base_url="http://ec2-34-245-111-10.eu-west-1.compute.amazonaws.com:5001/get-chitchat"
 # base_url="http://18.203.115.216:5007/api/v1/reranking"
 
-qry = 'Do you ever dream'
+qry = 'help me quit smoking'
 
 params = {
     "query": qry,
